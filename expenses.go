90d95e2ca3516f54ba2e8c32e97460746915f3f8
konfortes <ronen.konfortes@gettitan.io>
2019-01-18 20:44:57 +0200

    db persistance

--- expenses.go
+++ expenses.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func persistExpense(amount float32, description string, userID int) {
 	fmt.Printf("got an expense: amount: %f, description: %s\n", amount, description)
-	app.DB.Exec("INSERT INTO expenses(amount, category, user_id, description) VALUES($1, $2, $3, $4)", amount, "category", userID, description)
+	_, err := app.DB.Exec("INSERT INTO expenses(amount, category, user_id, description) VALUES($1, $2, $3, $4)", amount, "category", userID, description)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
